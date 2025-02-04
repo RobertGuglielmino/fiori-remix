6acc56ec3bfef28755ca302e275f2cedee6d0054
Robert Guglielmino <robert.gugliel@gmail.com>
2025-02-03 23:59:53 -0500

    intermediate header fixes

--- app/components/headers/HeaderContainer.tsx
+++ app/components/headers/HeaderContainer.tsx
@@ -10,18 +10,19 @@ interface HeaderContainerProps {
     amountLost: number;
     amountSaved: number;
     changeValue: number;
+    nextAction: string;
     // packSetTypes: any;
 }
 
-function HeaderContainer({ amountLost, amountSaved, changeValue }: HeaderContainerProps) {
+function HeaderContainer({ amountLost, amountSaved, nextAction, changeValue }: HeaderContainerProps) {
     return (
-        <div className="place-content-between border-black border-2 flex items-center flex-row gap-6 mx-4">
-            <button className='flex-basis-0 bg-red-500 size-12 rounded'> <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#FFFFFF"><path d="M266-200v-66.67h301.33q67.67 0 116.84-44.33 49.16-44.33 49.16-110.33t-49.16-110.34Q635-576 567.33-576H286.67l110.66 110.67-46.66 46.66L160-609.33 350.67-800l46.66 46.67-110.66 110.66h280q95.66 0 164.5 63.67Q800-515.33 800-421.33q0 94-68.83 157.66Q662.33-200 566.67-200H266Z"/></svg> </button>
+        <div className="place-content-evenly     border-black border-2 flex items-center flex-row gap-6 mx-4">
+            {/* <button className='flex-basis-0 bg-red-500 size-12 rounded'> <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#FFFFFF"><path d="M266-200v-66.67h301.33q67.67 0 116.84-44.33 49.16-44.33 49.16-110.33t-49.16-110.34Q635-576 567.33-576H286.67l110.66 110.67-46.66 46.66L160-609.33 350.67-800l46.66 46.67-110.66 110.66h280q95.66 0 164.5 63.67Q800-515.33 800-421.33q0 94-68.83 157.66Q662.33-200 566.67-200H266Z"/></svg> </button> */}
             <ValueLostBox value={amountLost} addValue={changeValue} />
             <FlipRipDisplay>FLIP IT OR RIP IT</FlipRipDisplay>
             <ValueSavedBox value={amountSaved} />
 
-            <PackSelector />
+            {/* <PackSelector /> */}
         </div>
     );
 };
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,42 @@
+import { json } from "@remix-run/node";
+import PackSelector from "../components/headers/pieces/PackSelector"
+import invariant from "tiny-invariant";
+import packTypesJson from './packTypes.json';
+import { useOutletContext } from "@remix-run/react";
+
+export async function loader() {
+  const response = await fetch('https://api.scryfall.com/sets');
+  const data = await response.json();
+  invariant(data, "Missing data from scryfall");
+
+  const packSetTypes = ['core', 'expansion', 'draft_innovation', 'masters', 'funny', 'remastered'];
+
+  // Filter sets that can be purchased as packs
+  const packSets = data.data
+    .filter((set: any) => packSetTypes.includes(set.set_type))
+    .filter((set: any) => set.code.length === 3)
+    .filter((set: any) => Object.keys(packTypesJson).includes(set.code.toUpperCase()))
+    .map((set: any) => {
+      return {
+        setCode: set.code.toUpperCase(),
+        setName: set.name
+      }
+    });
+  return json(packSets);
+}
 
 export default function Index() {
+  const outletContext = useOutletContext();
   // const packSetTypes = ['core', 'expansion', 'draft_innovation', 'masters', 'funny', 'remastered'];
-  const helperText = 
-  <>
-    <b>What?</b> Flip It Or Rip It is played by opening a pack of trading cards, placing them face down, alternating between Flipping a card face up (where you keep the card) and Ripping a card, so that it is destroyed forever.<br /><br />
-    <b>Why?</b> Someone thought that the rush of opening packs wasn't enough, so they adding in the potential of "losing money".<br /><br />
-    <b>How?</b> Pick some options from the drop down menus, generate the booster pack, and start clicking! The first card we will FLIP.
-  </>;
+  const helperText =
+    <>
+      <div className="center">
+        <PackSelector/> 
+      </div>
+      <b>What?</b> Flip It Or Rip It is played by opening a pack of trading cards, placing them face down, alternating between Flipping a card face up (where you keep the card) and Ripping a card, so that it is destroyed forever.<br /><br />
+      <b>Why?</b> Someone thought that the rush of opening packs wasn't enough, so they adding in the potential of "losing money".<br /><br />
+      <b>How?</b> Pick some options from the drop down menus, generate the booster pack, and start clicking! The first card we will FLIP.
+    </>;
 
   return (
     <div className="center m-10">
